4022dd76c7fb939f8d52c2ccb81a85feca4dcdf8
Porolilepishechka <124508160+Porolilepishechka@users.noreply.github.com>
2023-03-03 15:57:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+   $title = "головна";
+   require("blocks/header.php");
+?>
+<h1>головна сторінка</h1>
+<?php
+   require("blocks/footer.php");
+?>
---
+++ page1.php
@@ -0,0 +1,8 @@
+<?php
+   $title = "про сайт";
+   require("blocks/header.php");
+?>
+<h1>про сайт</h1>
+<?php
+   require("blocks/footer.php");
+?>
