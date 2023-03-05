9eff117f8ffbbebeffbac37f327263bd7794ba07
Porolilepishechka <124508160+Porolilepishechka@users.noreply.github.com>
2023-03-05 20:39:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-
 <?php
    $title = "головна";
    require("blocks/header.php");
 ?>
+<div class="container mt">
 <h1>головна сторінка</h1>
+   <form action="register.php" method="post">
+   <input type="email" name="email" placeholder="введіть email" class="form-control"><br>
+      <input type="text" name="user_name" placeholder="введіть імя" class="form-control"><br>
+      <input type="password" name="password" placeholder="введіть пароль" class="form-control"><br>
+      <button type="submit" style = "width: 160px; height:40px;" class="btn btn-success">
+      <b>зарегеструватися</b>
+   </form>
+</div>
 <?php
    require("blocks/footer.php");
-
 ?>
