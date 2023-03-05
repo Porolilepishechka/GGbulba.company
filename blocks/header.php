3753457b46d360db607d915439964bf441c216ec
Porolilepishechka <124508160+Porolilepishechka@users.noreply.github.com>
2023-03-05 20:49:39 +0300

    Update header.php

--- blocks/header.php
+++ blocks/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body> 
-<b><a href="index.php">головна</a></b>
-<b><a href="page1.php">про сайт</a></b>
-<b><a href="page2.php">калькулятор</a></b>
+<b><a href="index.php">головна</a></b> |
+<b><a href="page1.php">про сайт</a></b> |
+<b><a href="page2.php">калькулятор</a></b> |
 <b><?php echo "вітаємо " . $_COOKIE['user_name'];?></b>
